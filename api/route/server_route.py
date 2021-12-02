113ec818fea475f49cd2da4621833822171f1fa1
Samuel <samuel@alygne.me>
2021-12-02 15:50:28 +0100

    Implement CPU compute and computation feedback

--- api/route/server_route.py
+++ api/route/server_route.py
@@ -1,4 +1,5 @@
 import json
+from copy import copy
 
 from flask import Blueprint, request
 from flask_pydantic import validate
@@ -12,22 +13,21 @@ server_api = Blueprint('server_api', __name__, url_prefix='/v1/server')
 
 @server_api.route('/ref_data', methods=['POST'])
 @validate()
-def server_impact_ref_data(server: Server):
+def server_impact_ref_data(body: Server):
     # TODO: returns the closest server impact in the referenced data of Boavizta
     # server = server_mapper(request.json)
     #impacts = ref_data_server(server).to_json()
     print("\n\n Method : ref data \n\n")
-    impacts = ref.ref_data_server(server).to_json()
-    
-    
+    impacts = ref.ref_data_server(body).to_json()
     return impacts
 
 
 @server_api.route('/bottom-up', methods=['POST'])
 @validate()
 def server_impact_bottom_up(body: Server):
+    input_ = copy(body)
     impacts = bottom_up_server(body).to_json()
-    return impacts
+    return {'impacts': impacts, 'enriched_data': body.dict()}
 
 
 @server_api.route('/', methods=['POST'])
