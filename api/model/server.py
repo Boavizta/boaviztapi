af72d4b5068b0fb0dfe83fd6d057d5524205489d
Samuel <samuel@alygne.me>
2021-12-02 12:07:42 +0100

    Use pydantic model for server

--- api/model/server.py
+++ api/model/server.py
@@ -2,52 +2,59 @@ import json
 from pydantic import BaseModel
 from typing import Optional, List
 
-class Model_server(BaseModel):
+
+class ModelServer(BaseModel):
     manufacturer: Optional[str] = None 
     name: Optional[str] = None 
     type_: Optional[str] = None 
-    year:Optional[str] = None 
+    year: Optional[str] = None
 
-class Power_supply(BaseModel):
-    units :Optional[str] = None 
-    unit_weight :Optional[str] = None 
+
+class PowerSupply(BaseModel):
+    units: Optional[str] = None
+    unit_weight: Optional[str] = None
+
 
 class Disk(BaseModel):
-    units :Optional[str] = None 
-    type_ : Optional[str] = None 
-    capacity :Optional[str] = None 
-    density :Optional[str] = None 
-    manufacturer: Optional[str] = None 
-    manuf_date: Optional[str] = None 
-    model : Optional[str] = None  
+    units: Optional[str] = None
+    type_: Optional[str] = None
+    capacity: Optional[str] = None
+    density: Optional[str] = None
+    manufacturer: Optional[str] = None
+    manuf_date: Optional[str] = None
+    model: Optional[str] = None
+
 
 class Ram(BaseModel):
-    units:Optional[str] = None 
-    capacity:Optional[str] = None 
-    density:Optional[str] = None 
-    manufacturer : Optional[str] = None 
-    manuf_date : Optional[str] = None 
-    model: Optional[str] = None 
-    integrator : Optional[str] = None 
+    units: Optional[str] = None
+    capacity: Optional[str] = None
+    density: Optional[str] = None
+    manufacturer: Optional[str] = None
+    manuf_date: Optional[str] = None
+    model: Optional[str] = None
+    integrator: Optional[str] = None
+
 
 class Cpu(BaseModel):
-    units :Optional[str] = None 
-    core_units:Optional[str] = None 
-    die_size :Optional[str] = None 
-    manufacturer : Optional[str] = None 
-    manuf_date : Optional[str] = None 
-    model : Optional[str] = None 
-    cpu_family: Optional[str] = None 
+    units: Optional[str] = None
+    core_units: Optional[str] = None
+    die_size: Optional[str] = None
+    manufacturer: Optional[str] = None
+    manuf_date: Optional[str] = None
+    model: Optional[str] = None
+    cpu_family: Optional[str] = None
 
-class Configuration_server(BaseModel):
-    cpu : Optional[Cpu] = None 
-    ram : Optional[List[Ram]] = None
-    disk : Optional[List[Disk]] = None 
-    power_supply : Optional[Power_supply] = None
+
+class ConfigurationServer(BaseModel):
+    cpu: Optional[Cpu] = None
+    ram: Optional[List[Ram]] = None
+    disk: Optional[List[Disk]] = None
+    power_supply: Optional[PowerSupply] = None
+
 
 class Server(BaseModel):
-    model : Model_server 
-    configuration : Configuration_server 
-    
-    add_method : Optional[str] = None 
-    add_date : Optional[str] = None 
+    model: ModelServer
+    configuration: ConfigurationServer
+
+    add_method: Optional[str] = None
+    add_date: Optional[str] = None
--- api/route/server_route.py
+++ api/route/server_route.py
@@ -1,8 +1,9 @@
 import json
 
 from flask import Blueprint, request
+from flask_pydantic import validate
 
-from api.model.server import server_mapper
+from api.model.server import Server
 from api.service.server_impact.bottom_up.bottom_up import bottom_up_server
 from api.service.server_impact.ref import ref
 
@@ -10,9 +11,10 @@ server_api = Blueprint('server_api', __name__, url_prefix='/v1/server')
 
 
 @server_api.route('/ref_data', methods=['POST'])
-def server_impact_ref_data():
+@validate()
+def server_impact_ref_data(server: Server):
     # TODO: returns the closest server impact in the referenced data of Boavizta
-    server = server_mapper(request.json)
+    # server = server_mapper(request.json)
     #impacts = ref_data_server(server).to_json()
     print("\n\n Method : ref data \n\n")
     impacts = ref.ref_data_server(server).to_json()
@@ -22,9 +24,10 @@ def server_impact_ref_data():
 
 
 @server_api.route('/bottom-up', methods=['POST'])
-def server_impact_bottom_up():
+@validate()
+def server_impact_bottom_up(server: Server):
     # create server object
-    server = server_mapper(request.json)
+    # server = server_mapper(request.json)
     # apply bottom_up methodology
     impacts = bottom_up_server(server).to_json()
     return impacts
