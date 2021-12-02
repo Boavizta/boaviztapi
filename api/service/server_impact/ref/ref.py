f287f6a60dd797c562ea98c19189637ef1b851b6
benjaminlebigot <benjaminlebigot@gmail.com>
2021-12-02 18:33:18 +0100

    end of the day hackthon 2/12/2021

--- api/service/server_impact/ref/ref.py
+++ api/service/server_impact/ref/ref.py
@@ -2,6 +2,17 @@ from api.model.impacts import Impact, Impacts
 import pandas as pd
 import os
 def ref_data_server(server, impact_codes=None):
-
-    pd.read_csv()
+    obj_input = 
+    db =  pd.read_csv(boavizta_db)
     return
+
+
+
+'''
+search :
+
+model : (name) - done 
+
+
+
+'''
