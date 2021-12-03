7d7e36efb61c7b144b6a9bff68e409adea6e3931
benjaminlebigot <benjaminlebigot@gmail.com>
2021-12-03 11:47:54 +0100

    #1  ref_data route, debugging

--- api/service/server_impact/ref/ref.py
+++ api/service/server_impact/ref/ref.py
@@ -1,14 +1,15 @@
 from api.model.impacts import Impact, Impacts
 
 import pandas as pd
+import re
 import os
 
 
 
 def ref_data_server(server):
-    database_file = "boavizta-data-us.csv"
+    database_file = "./api/service/server_impact/ref/boavizta-data-us.csv"
     database = pd.read_csv(database_file)
-    df = database[database['name'].str.contains(server.model.name, regex=False)]
+    df = database[database['name'].str.contains(server.model.name, regex=False, flags=re.IGNORECASE)]
     if(len(df) >0):
         # add a scope 3 computation
         df['scope_3'] = df.apply(lambda row: row['gwp_total'] * row['gwp_manufacturing_ratio'], axis=1)
