ffac600a81b95b75e34e6d88d0264c9eaa7703af
da-ekchajzer <da.ekchajzer@gmail.com>
2022-01-03 17:45:40 +0100

    #1 ref data

--- api/model/components/usage.py
+++ api/model/components/usage.py
@@ -1,4 +1,3 @@
-import string
 from abc import abstractmethod
 
 import pandas as pd
--- api/service/ref_data.py
+++ api/service/ref_data.py
@@ -1,7 +1,11 @@
 from api.dto.server_dto import ServerDTO
-from api.model.devices.device import Server
+from api.model.devices.device import Server, Device
 
 
 def ref_server(server_dto: ServerDTO) -> Server:
 
-    return server_dto.to_device()
+    """
+    TODO find server by name, year, brand, ...
+        replace the configuration variable sent by the user
+        return server
+       """
