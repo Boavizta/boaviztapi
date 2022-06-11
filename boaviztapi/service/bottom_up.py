ad6e58b8bd9178d20578b1a50e04eb97fba74f3e
Samuel Rince <s@rince.me>
2022-06-11 17:29:47 +0200

    refactor: use a more generic get_model_impact method to work with component and device objects

--- boaviztapi/service/bottom_up.py
+++ boaviztapi/service/bottom_up.py
@@ -8,56 +8,56 @@ import boaviztapi.utils.roundit as rd
 NOT_IMPLEMENTED = 'not implemented'
 
 
-def bottom_up_device(device: Device) -> dict:
-    impacts = {
-        'gwp': {
-            'manufacture': rd.round_to_sigfig(*device.impact_manufacture_gwp()),
-            'use': rd.round_to_sigfig(*device.impact_use_gwp()),
-            'unit': "kgCO2eq"
-        },
-        'pe': {
-            'manufacture': rd.round_to_sigfig(*device.impact_manufacture_pe()),
-            'use': rd.round_to_sigfig(*device.impact_use_pe()),
-            'unit': "MJ"
-        },
-        'adp': {
-            'manufacture': rd.round_to_sigfig(*device.impact_manufacture_adp()),
-            'use': rd.round_to_sigfig(*device.impact_use_adp()),
-            'unit': "kgSbeq"
-        },
-    }
-    return impacts
-
-
 def bottom_up_component(component: Component, units: int = 1) -> dict:
     impacts = {
         'gwp': {
-            'manufacture': get_component_impact(component, 'manufacture', 'gwp', units) or NOT_IMPLEMENTED,
-            'use': get_component_impact(component, 'use', 'pe', units) or NOT_IMPLEMENTED,
+            'manufacture': get_model_impact(component, 'manufacture', 'gwp', units) or NOT_IMPLEMENTED,
+            'use': get_model_impact(component, 'use', 'pe', units) or NOT_IMPLEMENTED,
             'unit': "kgCO2eq"
         },
         'pe': {
-            'manufacture': get_component_impact(component, 'manufacture', 'pe', units) or NOT_IMPLEMENTED,
-            'use': get_component_impact(component, 'use', 'pe', units) or NOT_IMPLEMENTED,
+            'manufacture': get_model_impact(component, 'manufacture', 'pe', units) or NOT_IMPLEMENTED,
+            'use': get_model_impact(component, 'use', 'pe', units) or NOT_IMPLEMENTED,
             'unit': "MJ"
         },
         'adp': {
-            'manufacture': get_component_impact(component, 'manufacture', 'adp', units) or NOT_IMPLEMENTED,
-            'use': get_component_impact(component, 'use', 'adp', units) or NOT_IMPLEMENTED,
+            'manufacture': get_model_impact(component, 'manufacture', 'adp', units) or NOT_IMPLEMENTED,
+            'use': get_model_impact(component, 'use', 'adp', units) or NOT_IMPLEMENTED,
             'unit': "kgSbeq"
         },
     }
     return impacts
 
 
-def get_component_impact(component: Component,
-                         phase: str,
-                         impact_type: str,
-                         units: int) -> Optional[float]:
+def get_model_impact(model: Union[Component, Device],
+                     phase: str,
+                     impact_type: str,
+                     units: int = 1) -> Optional[float]:
     try:
-        impact_function = component.__getattribute__(f'impact_{phase}_{impact_type}')
+        impact_function = model.__getattribute__(f'impact_{phase}_{impact_type}')
         impact, significant_figures = impact_function()
         units_impact = impact * units
         return rd.round_to_sigfig(units_impact, significant_figures)
     except (AttributeError, NotImplementedError):
         pass
+
+
+def bottom_up_device(device: Device) -> dict:
+    impacts = {
+        'gwp': {
+            'manufacture': get_model_impact(device, 'manufacture', 'gwp') or NOT_IMPLEMENTED,
+            'use': get_model_impact(device, 'use', 'gwp') or NOT_IMPLEMENTED,
+            'unit': "kgCO2eq"
+        },
+        'pe': {
+            'manufacture': get_model_impact(device, 'manufacture', 'pe') or NOT_IMPLEMENTED,
+            'use': get_model_impact(device, 'use', 'pe') or NOT_IMPLEMENTED,
+            'unit': "MJ"
+        },
+        'adp': {
+            'manufacture': get_model_impact(device, 'manufacture', 'adp') or NOT_IMPLEMENTED,
+            'use': get_model_impact(device, 'use', 'adp') or NOT_IMPLEMENTED,
+            'unit': "kgSbeq"
+        },
+    }
+    return impacts
