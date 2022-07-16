3792f25650504af66f43c8050727cd9607b238ab
Samuel Rince <s@rince.me>
2022-07-16 16:25:10 +0200

    style: rename and simplify

--- boaviztapi/dto/consumption_profile/consumption_profile.py
+++ boaviztapi/dto/consumption_profile/consumption_profile.py
@@ -27,15 +27,14 @@ class ConsumptionProfileCPU(ConsumptionProfile):
 
 
 def smart_mapper_consumption_profile_cpu(cp_cpu_dto: ConsumptionProfileCPU) -> CPUConsumptionProfileModel:
-    cpu_manufacturer, cpu_sub_name = parse(cp_cpu_dto.cpu.name)
-    sub = _cpu_index
+    cpu_manufacturer, cpu_sub_name = parse_cpu_name(cp_cpu_dto.cpu.name)
     if cpu_manufacturer is None:
-        name_list = list(sub.sub_model_name.unique())
+        name_list = list(_cpu_index.sub_model_name.unique())
     else:
-        name_list = list(sub[sub['manufacturer'] == cpu_manufacturer].sub_model_name.unique())
-    result = fuzzymatch(cpu_sub_name, name_list)
+        name_list = list(_cpu_index[_cpu_index['manufacturer'] == cpu_manufacturer].sub_model_name.unique())
+    result = match_cpu_model_range(cpu_sub_name, name_list)
     if result is not None:
-        model_range = sub[sub['sub_model_name'] == result[0]].iloc[0].model_range
+        model_range = _cpu_index[_cpu_index['sub_model_name'] == result[0]].iloc[0].model_range
     else:
         model_range = None
 
@@ -45,18 +44,16 @@ def smart_mapper_consumption_profile_cpu(cp_cpu_dto: ConsumptionProfileCPU) -> C
     return cpu_consumption_profile
 
 
-def parse(cpu_name: str) -> Tuple[str, str]:
+def parse_cpu_name(cpu_name: str) -> Tuple[str, str]:
     vendor_list = ["intel", "amd", "arm"]  # every string in lowercase
-    vendor_erased = None
     for vendor in vendor_list:
         if vendor in cpu_name:
             cpu_name.replace(vendor, '')
-            vendor_erased = vendor
             return vendor, cpu_name.replace(vendor, '')
     return None, cpu_name
 
 
-def fuzzymatch(cpu_name_to_match: str, cpu_name_list: list) -> Optional[Tuple[str, float, int]]:
+def match_cpu_model_range(cpu_name_to_match: str, cpu_name_list: list) -> Optional[Tuple[str, float, int]]:
     foo = process.extractOne(cpu_name_to_match, cpu_name_list, scorer=fuzz.WRatio)
     if foo is not None:
         return foo if foo[1] > 88.0 else None
