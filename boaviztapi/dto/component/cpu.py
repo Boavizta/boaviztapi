7ba2dcf1b06718807bd9338863bed34c1f85bd89
da-ekchajzer <david_ekchajzer@Laptop.dek>
2022-12-28 11:49:39 +0100

    updating cpu smart complete to get the closest die size per core from cpu core_units

--- boaviztapi/dto/component/cpu.py
+++ boaviztapi/dto/component/cpu.py
@@ -1,6 +1,7 @@
 import os
 from typing import Optional, Tuple
 
+import numpy as np
 import pandas as pd
 from rapidfuzz import fuzz, process
 
@@ -32,19 +33,27 @@ def smart_mapper_cpu(cpu_dto: CPU) -> ComponentCPU:
     cpu_component.units = cpu_dto.units
 
     if cpu_dto.name is not None:
-        manufacturer, model_range, family = attributes_from_cpu_name(cpu_dto.name)
+        manufacturer, model_range, family, tdp = attributes_from_cpu_name(cpu_dto.name)
         if manufacturer is not None:
             cpu_dto.manufacturer = manufacturer
             cpu_component.manufacturer.value = manufacturer
             cpu_component.manufacturer.status = Status.COMPLETED
+            cpu_component.manufacturer.source = "from name"
         if model_range is not None:
             cpu_dto.model_range = model_range
             cpu_component.model_range.value = model_range
             cpu_component.model_range.status = Status.COMPLETED
+            cpu_component.model_range.source = "from name"
         if family is not None:
             cpu_dto.family = family
             cpu_component.family.value = family
             cpu_component.family.status = Status.COMPLETED
+            cpu_component.family.source = "from name"
+        if tdp is not None:
+            cpu_dto.tdp = tdp
+            cpu_component.tdp.value = tdp
+            cpu_component.tdp.status = Status.COMPLETED
+            cpu_component.tdp.source = "from name"
 
     if cpu_dto.family is not None and cpu_component.family.status != Status.COMPLETED:
         cpu_component.family.value = cpu_dto.family
@@ -54,16 +63,20 @@ def smart_mapper_cpu(cpu_dto: CPU) -> ComponentCPU:
         cpu_component.core_units.value = cpu_dto.core_units
         cpu_component.core_units.status = Status.INPUT
 
-    if cpu_dto.tdp is not None:
+    if cpu_dto.tdp is not None and cpu_component.tdp.status != Status.COMPLETED:
         cpu_component.tdp.value = cpu_dto.tdp
         cpu_component.tdp.status = Status.INPUT
 
-    if cpu_dto.model_range is not None:
+    if cpu_dto.model_range is not None and cpu_component.model_range.status != Status.COMPLETED:
         cpu_component.model_range.value = cpu_dto.model_range
         cpu_component.model_range.status = Status.INPUT
 
     if cpu_dto.die_size_per_core is not None:
         cpu_component.die_size_per_core.value = cpu_dto.die_size_per_core
+        cpu_component.die_size_per_core.status = Status.INPUT
+
+    if cpu_dto.die_size_per_core is not None:
+        cpu_component.core_units.value = cpu_dto.core_units
         cpu_component.die_size_per_core.status = Status.INPUT
 
     elif cpu_dto.die_size is not None and cpu_dto.core_units is not None:
@@ -82,42 +95,31 @@ def smart_mapper_cpu(cpu_dto: CPU) -> ComponentCPU:
             if len(tmp) > 0:
                 sub = tmp.copy()
 
-        if cpu_dto.core_units is not None:
-            tmp = sub[sub['core_units'] == cpu_dto.core_units]
-            if len(tmp) > 0:
-                sub = tmp.copy()
-
         if len(sub) == 0 or len(sub) == len(_cpu_df):
             pass
-        elif len(sub) == 1:
-            cpu_component.die_size_per_core.value = float(sub['die_size_per_core'])
-            cpu_component.die_size_per_core.status = Status.COMPLETED
-            cpu_component.die_size_per_core.source = str(sub['Source'].iloc[0])
-        else:
-            sub['_scope3'] = sub[['core_units', 'die_size_per_core']].apply(lambda x: x[0] * x[1], axis=1)
-            sub = sub.sort_values(by='_scope3', ascending=False)
+
+        elif cpu_dto.core_units is not None:
+            # Find the closest line to the number of cores provided by the user
+            sub['core_dif'] = sub[['core_units']].apply(lambda x: abs(x[0] - cpu_dto.core_units), axis=1)
+            sub = sub.sort_values(by='core_dif', ascending=True)
             row = sub.iloc[0]
 
             cpu_component.die_size_per_core.value = float(row['die_size_per_core'])
+            cpu_component.die_size_per_core.status = Status.COMPLETED
             cpu_component.die_size_per_core.source = row['Source']
 
-            if cpu_dto.die_size_per_core is None:
-                cpu_component.die_size_per_core.status = Status.COMPLETED
-            else:
-                cpu_component.die_size_per_core.status = Status.CHANGED
-
-            cpu_component.core_units.value = int(row['core_units'])
-            cpu_component.core_units.source = row['Source']
-
-            if cpu_dto.core_units is None:
-                cpu_component.core_units.status = Status.COMPLETED
-            else:
-                cpu_component.core_units.status = Status.CHANGED
+        else:
+            # Maximize die_size_per_core
+            sub = sub.sort_values(by='die_size_per_core', ascending=False)
+            row = sub.iloc[0]
+            cpu_component.die_size_per_core.value = float(row['die_size_per_core'])
+            cpu_component.die_size_per_core.status = Status.COMPLETED
+            cpu_component.die_size_per_core.source = row['Source'] + "- maximizing value without core_unit given"
 
     return cpu_component
 
 
-def attributes_from_cpu_name(cpu_name: str) -> [str, str, str]:
+def attributes_from_cpu_name(cpu_name: str) -> [str, str, str, int]:
     cpu_name = cpu_name.lower()
     manufacturer, cpu_sub_name = parse(cpu_name)
     sub = _cpu_index
@@ -126,14 +128,20 @@ def attributes_from_cpu_name(cpu_name: str) -> [str, str, str]:
     else:
         name_list = list(sub[sub['manufacturer'] == manufacturer].sub_model_name.unique())
     result = fuzzymatch(cpu_sub_name, name_list)
+
     if result is not None:
         model_range = sub[sub['sub_model_name'] == result[0]].iloc[0].model_range
         family = sub[sub['sub_model_name'] == result[0]].iloc[0].family
+        tdp = sub[sub['sub_model_name'] == result[0]].iloc[0].tdp
+        if np.isnan(tdp):
+            tdp = None
+
     else:
         model_range = None
         family = None
+        tdp = None
 
-    return manufacturer, model_range, family
+    return manufacturer, model_range, family, tdp
 
 
 def parse(cpu_name: str) -> Tuple[str, str]:
