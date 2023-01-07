8ec4883a369515ef9f60b8ac6b6d2a8aeb46e0a4
da-ekchajzer <david_ekchajzer@Laptop.dek>
2023-01-07 18:04:58 +0100

    creating an impact object

--- boaviztapi/model/impact.py
+++ boaviztapi/model/impact.py
@@ -1,13 +1,36 @@
+from dataclasses import dataclass
+import boaviztapi.utils.roundit as rd
+
+@dataclass
+class ImpactCriteria:
+    name: str
+    unit: str
+    description: str
 class Impact:
     def __init__(self, **kwargs):
         self.value = 0
-        self.unit = "none"
+        self.significant_figures = 1
         self.error_margin = 0
         self.warnings = []
 
         for attr, val in kwargs.items():
             if val is not None:
                 self.__setattr__(attr, val)
-
     def add_warning(self, warn):
-        self.warnings = self.warnings.append(warn)
+        self.warnings.append(warn)
+    def to_json(self):
+        json = {"value": rd.round_to_sigfig(self.value, self.significant_figures), "significant_figures": self.significant_figures}
+        if self.error_margin: json['error_margin'] = self.error_margin
+        if self.warnings: json['warnings'] = self.warnings
+
+        return json
+    def rounded_value(self):
+        return rd.round_to_sigfig(self.value, self.significant_figures)
+
+
+GWP = ImpactCriteria(name="gwp", unit="kgCO2eq", description="Effects on global warming")
+ADP = ImpactCriteria(name="adp", unit="kgSbeq", description="Use of minerals and fossil ressources")
+PE = ImpactCriteria(name="pe", unit="MJ", description="Consumption of primary energy")
+
+IMPACT_CRITERIAS = [GWP, ADP, PE]
+IMPACT_PHASES = ["manufacture", "use"]
