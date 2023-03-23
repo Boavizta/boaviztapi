c89648cc873d2320c542e357804ca07ef84f3b6b
da-ekchajzer <david_ekchajzer@Laptop.dek>
2023-03-23 16:42:41 +0100

    adding get all names utils router + correcting cloud utils routers

--- boaviztapi/routers/cloud_router.py
+++ boaviztapi/routers/cloud_router.py
@@ -1,4 +1,3 @@
-import copy
 import os
 import pandas as pd
 
@@ -7,9 +6,8 @@ from fastapi import APIRouter, Query, Body, HTTPException
 from boaviztapi import config, data_dir
 from boaviztapi.dto.device import Cloud
 from boaviztapi.dto.device.device import mapper_cloud_instance
-from boaviztapi.dto.usage import UsageCloud
 from boaviztapi.routers.openapi_doc.descriptions import cloud_provider_description, all_default_cloud_instances, all_default_cloud_providers, cloud_aws_description, all_default_aws_instances
-from boaviztapi.routers.openapi_doc.examples import cloud_usage_example, cloud_example
+from boaviztapi.routers.openapi_doc.examples import cloud_example
 from boaviztapi.routers.server_router import server_impact
 from boaviztapi.service.allocation import Allocation
 from boaviztapi.service.archetype import get_cloud_instance_archetype, get_device_archetype_lst
@@ -18,33 +16,6 @@ cloud_router = APIRouter(
     prefix='/v1/cloud',
     tags=['cloud']
 )
-
-
-@cloud_router.post('/aws',
-                   description=cloud_aws_description)
-async def legacy_instance_cloud_impact(cloud_usage: UsageCloud = Body(None, example=cloud_usage_example),
-                                instance_type: str = Query(None, example="a1.4xlarge"), verbose: bool = True,
-                                allocation: Allocation = Allocation.TOTAL):
-    cloud_instance = Cloud()
-    cloud_instance.usage = cloud_usage
-    instance_archetype = get_cloud_instance_archetype(instance_type, "aws")
-
-    if not instance_archetype:
-        raise HTTPException(status_code=404, detail=f"{instance_type} not found")
-
-    instance_model = mapper_cloud_instance(cloud_instance, archetype=instance_archetype)
-
-    return await server_impact(
-        device=instance_model,
-        verbose=verbose,
-        allocation=allocation
-    )
-
-
-@cloud_router.get('/aws/all_instances',
-                  description=all_default_aws_instances)
-async def legacy_server_get_all_archetype_name():
-    return get_device_archetype_lst(os.path.join(data_dir, 'devices/cloud/aws.csv'))
 
 @cloud_router.post('/',
                    description=cloud_provider_description)
@@ -88,12 +59,12 @@ async def instance_cloud_impact(provider: str = Query(config["default_cloud_prov
 @cloud_router.get('/all_instances',
                   description=all_default_cloud_instances)
 async def server_get_all_archetype_name(provider: str = Query(None, example="aws")):
-    if not os.path.exists(data_dir +'/devices/cloud/' + provider + '.csv'):
+    if not os.path.exists(data_dir +'/archetypes/cloud/' + provider + '.csv'):
         raise HTTPException(status_code=404, detail=f"No available data for this cloud provider ({provider})")
-    return get_device_archetype_lst(os.path.join(data_dir, 'devices/cloud/' + provider + '.csv'))
+    return get_device_archetype_lst(os.path.join(data_dir, 'archetypes/cloud/' + provider + '.csv'))
 
 @cloud_router.get('/all_providers',
                   description=all_default_cloud_providers)
 async def server_get_all_provider_name():
-    df = pd.read_csv(os.path.join(data_dir, 'devices/cloud/providers.csv'))
+    df = pd.read_csv(os.path.join(data_dir, 'archetypes/cloud/providers.csv'))
     return df['provider.name'].tolist()
