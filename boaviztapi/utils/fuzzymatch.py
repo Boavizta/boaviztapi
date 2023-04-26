b4f9817661da518eb371439d0ddcaa831e2559ac
Christophe Saugé <csauge@gmail.com>
2023-04-26 14:12:23 +0200

    Fix issues in fuzzymatch
    - Suppress 75 duplications in cpu_specs.csv
    - Simplify and speed-up the fuzzy match by removing frequency
    - Update rapidfuzz version to 3.0.0
    - Remove an incorrect unit test, fix one and add two more

--- boaviztapi/utils/fuzzymatch.py
+++ boaviztapi/utils/fuzzymatch.py
@@ -4,12 +4,23 @@ from rapidfuzz import process, fuzz
 from typing import Tuple
 
 
-def fuzzymatch_attr_from_cpu_name(cpu_name: str, df) -> Tuple[str, str, str, str, int, int, int, str, str]:
-    score = lambda x: fuzz.token_set_ratio(f'{x["name"]} {"" if pd.isna(x["frequency"]) else x["frequency"]}'.lower(), cpu_name.lower())  # token_set_ratio seems a bit better than ratio
-    best = df.iloc[df.apply(score, axis=1).idxmax()]  # Find the best fuzzy match
-    best = best.where(pd.notna(best), None)  # Replace all NaN by None
-    safe_int = lambda x: x if x is None else int(x)  # Cast from float to int but keep None values
-    return best["name"], best.manufacturer, best.code_name, best.model_range, safe_int(best.tdp), safe_int(best.cores), safe_int(best.total_die_size), best.total_die_size_source, best.source  # best.name is reserved by pandas for indexes
+def fuzzymatch_attr_from_cpu_name(cpu_name: str, df: pd.DataFrame) -> Tuple[str, str, str, str, int, int, int, str, str]:
+    cpu_name = cpu_name.lower()
+    score = df["name"].str.lower().apply(lambda x: fuzz.token_set_ratio(x, cpu_name))
+    best = df.iloc[score.idxmax()]
+    best = best.mask(best.isnull(), None)  # replace all NaN by None
+    safe_int = lambda x: x if x is None else int(x)  # float to int but keep None
+    return (
+        best["name"],  # .name is reserved by pandas for indexes
+        best.manufacturer,
+        best.code_name,
+        best.model_range,
+        safe_int(best.tdp),
+        safe_int(best.cores),
+        safe_int(best.total_die_size),
+        best.total_die_size_source,
+        best.source,
+    )
 
 
 def fuzzymatch_attr_from_pdf(name: str, attr: str, pdf: pd.DataFrame) -> str:
@@ -17,11 +28,11 @@ def fuzzymatch_attr_from_pdf(name: str, attr: str, pdf: pd.DataFrame) -> str:
 
     result = process.extractOne(name, name_list, scorer=fuzz.WRatio)
     if result is not None:
-        result = result[0] if result[1] > 88.0 else None
+        result = result[0] if result[1] > 79.0 else None
     return result or None
 
 
-def fuzzymatch(s: pd.Series, value: str, threshold: float = 90.) -> pd.Series:
+def fuzzymatch(s: pd.Series, value: str, threshold: float = 90.0) -> pd.Series:
     return s.apply(lambda x: fuzz.token_sort_ratio(x.lower(), value.lower()) >= threshold)
 
 
