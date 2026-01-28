2ee464aabad31176416d6cbb0708d83490dcb014
Simon Shillaker <mail@simonshillaker.com>
2026-01-28 11:26:05 +0100

    usage: update main module init

--- boaviztapi/__init__.py
+++ boaviztapi/__init__.py
@@ -1,13 +1,12 @@
 import os
 import sys
-from pathlib import Path
 
-import yaml
+from boaviztapi.utils.config import config
 
+# Use test data directory when running under pytest
 if "pytest" in sys.modules:
-    data_dir = os.path.join(os.path.dirname(__file__), "../tests/data")
+    data_dir = os.path.join(os.path.dirname(__file__), "..", "tests", "data")
 else:
     data_dir = os.path.join(os.path.dirname(__file__), "data")
 
-config_file = os.path.join(data_dir, "config.yml")
-config = yaml.safe_load(Path(config_file).read_text())
+__all__ = ["config", "data_dir"]
