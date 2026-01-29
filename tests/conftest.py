11290725b68418a9332a28b3e7be15053f37b91d
Simon Shillaker <mail@simonshillaker.com>
2026-01-29 14:18:27 +0100

    refactor: change to use new config

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,26 +1,44 @@
+import sys
+
 import pytest
 
+from boaviztapi import config as bv_config
 
-# All top-level pytest configuration
+# Check if we're running end-to-end tests
+_running_e2e = "--rune2e" in sys.argv
+
+# Add overrides for non-E2E tests
+if not _running_e2e:
+    bv_config.cpu_name_fuzzymatch_threshold = 60
+    bv_config.default_case = "DEFAULT"
+    bv_config.default_server = "DEFAULT"
+
+
 def pytest_configure(config):
-    # E2E marker to mark tests as E2E
-    config.addinivalue_line("markers", "e2e: mark test as an e2e test")
+    """Applies all top-level pytest configuration."""
+
+    # Allow marking end-to-end tests with annotation
+    config.addinivalue_line("markers", "e2e: mark test as an end-to-end test")
 
 
-# Add option to run only E2E tests. If not specified, all non-E2E tests will run
 def pytest_addoption(parser):
+    """Adds global options to the pytest commandline."""
+
+    # Option to run only end-to-end tests. If not specified, all non-E2E tests will run
     parser.addoption(
         "--rune2e",
         action="store_true",
         default=False,
-        help="Run e2e tests",
+        help="Run end-to-end tests",
     )
 
 
 def pytest_collection_modifyitems(config, items):
-    # Run only E2E tests if --rune2e specified, otherwise exclude them
+    """Provides the list of tests for pytest to run."""
+
+    # Skip non-end-to-end tests if running end-to-end tests
     if config.getoption("--rune2e"):
-        skip_non_e2e = pytest.mark.skip(reason="--rune2e runs only e2e tests")
+        skip_non_e2e = pytest.mark.skip(reason="--rune2e runs only end-to-end tests")
         for item in items:
             if "e2e" not in item.keywords:
                 item.add_marker(skip_non_e2e)
