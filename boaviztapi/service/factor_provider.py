d1af31dd373ee45c503d8fe6ed08999c7ec09278
Simon Shillaker <mail@simonshillaker.com>
2026-02-01 17:01:42 +0100

    usage: port code to use new service

--- boaviztapi/service/factor_provider.py
+++ boaviztapi/service/factor_provider.py
@@ -3,6 +3,9 @@ from pathlib import Path
 
 import yaml
 from boaviztapi import data_dir
+from boaviztapi.service.electricitymaps import fetch_carbon_intensity
+from boaviztapi.utils.config import config
+from boaviztapi.utils.country import iso3_to_iso2
 
 config_file = os.path.join(data_dir, "factors.yml")
 impact_factors = yaml.load(Path(config_file).read_text(), Loader=yaml.CSafeLoader)
@@ -26,6 +29,14 @@ def get_gpu_impact_factor(component, phase, impact_type) -> dict:
 
 
 def get_electrical_impact_factor(usage_location, impact_type) -> dict:
+    if config.electricity_maps_api_key and impact_type == "gwp":
+        try:
+            zone = iso3_to_iso2(usage_location)
+            return fetch_carbon_intensity(config.electricity_maps_api_key, zone)
+        except ValueError:
+            # If we don't have a valid ISO3 location, continue
+            pass
+
     if impact_factors["electricity"].get(usage_location):
         if impact_factors["electricity"].get(usage_location).get(impact_type):
             return impact_factors["electricity"].get(usage_location).get(impact_type)
@@ -88,48 +99,3 @@ def get_iot_impact_factor(functional_block, hsl, impact_type):
                     .get(hsl)["eol"][impact_type]
                 )
     raise NotImplementedError
-
-
-"""
-_electricity_emission_factors_df = pd.read_csv(
-    os.path.join(data_dir, 'electricity/electricity_impact_factors.csv'))
-class ElecFactorProvider:
-    def get(self, criteria, location, date):
-        pass
-    def get_range(self, criteria, location, date1, date2):
-        pass
-class BoaviztaFactors(ElecFactorProvider):
-    def get(self, criteria, usage_location, date=None):
-        sub = _electricity_emission_factors_df
-        sub = sub[sub['code'] == usage_location]
-        return float(sub[f"{criteria}_emission_factor"]), sub[f"{criteria}_emission_source"].iloc[0], 0, ["The impact factor is averaged over the year"]
-    def get_range(self, criteria, usage_location, date1=None, date2=None):
-        return self.get(criteria,usage_location)
-
-class ElectricityMap(ElecFactorProvider):
-    auth_token = "6QGqlsF7ZcdUN6TMB7jX9DMsYKeGHbVl"
-    url = "https://api-access.electricitymaps.com/2w97h07rvxvuaa1g"
-    now = datetime.now()
-    def get(self, criteria, location, date):
-        zone = self._location_to_em_zone(location)
-        if self.now - timedelta(hours=1) < date < self.now + timedelta(hours=1):
-            return self._get_current(zone)
-        elif date < self.now:
-            return self._get_history(zone, date)
-        else:
-            return NotImplementedError
-    def get_range(self, criteria, zone, date1, date2):
-        pass
-    def _get_current(self, zone):
-        reponse = requests.get(f"{self.url}/carbon-intensity/latest?zone={zone}",
-                headers={"X-BLOBR-KEY": self.auth_token}).json()
-
-        return reponse["carbonIntensity"]/1000, f"electricity map response : {reponse}", 0, []
-    def _get_history(self, zone, date):
-        reponse = requests.get(f"{self.url}/carbon-intensity/history?zone={zone}&datetime={date}",
-                               headers={"X-BLOBR-KEY": self.auth_token }).json()
-
-        return reponse
-    def _location_to_em_zone(self, location):
-        return location
-"""
