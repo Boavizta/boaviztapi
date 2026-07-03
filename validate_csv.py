40b4b2e33d088960960f33346e32365f200a1fd0
olivier de Meringo <demeringo@gmail.com>
2026-07-03 12:33:15 +0200

    chore: format python

--- validate_csv.py
+++ validate_csv.py
@@ -56,7 +56,9 @@ for csv_path, _ in PROVIDER_CSV_FILES:
     if duplicates:
         has_duplicates = True
         print(f"Found {len(duplicates)} duplicate ID's in {csv_path.name}:")
-        print("Following entries needs to be verified ( duplicate id : number of occurences):")
+        print(
+            "Following entries needs to be verified ( duplicate id : number of occurences):"
+        )
         print(json.dumps(duplicates, indent=1))
 
 if has_duplicates:
